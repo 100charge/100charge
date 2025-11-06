c8eac29d82066e07c1a6751854389ca33e5eaaa1
岩 <antony-l@126.com>
2025-11-06 16:09:52 +0800

    remove unused  package

--- charging-api/xingchuan-common/src/main/java/com/xingchuan/common/core/domain/entity/SysMenu.java
+++ charging-api/xingchuan-common/src/main/java/com/xingchuan/common/core/domain/entity/SysMenu.java
@@ -6,7 +6,6 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import org.apache.commons.lang3.builder.ToStringBuilder;
