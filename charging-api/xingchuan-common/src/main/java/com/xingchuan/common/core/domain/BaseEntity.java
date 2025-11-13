d3899c9f8f7ef8c620252e567b8509651396e3e9
岩 <antony-l@126.com>
2025-11-13 14:17:58 +0800

    fix searchValue not exist

--- charging-api/xingchuan-common/src/main/java/com/xingchuan/common/core/domain/BaseEntity.java
+++ charging-api/xingchuan-common/src/main/java/com/xingchuan/common/core/domain/BaseEntity.java
@@ -1,5 +1,6 @@
 package com.xingchuan.common.core.domain;
 
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
@@ -21,6 +22,7 @@ public class BaseEntity implements Serializable {
      * 搜索值
      */
     @JsonIgnore
+    @TableField(exist = false)
     private String searchValue;
 
     /**
